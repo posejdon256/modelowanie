f2b82fe798eab335518992ec6a1c970485e913f4
Anna Bujak <aniutka.pop@gmail.com>
2018-09-07 11:10:56 +0200

    fix bug

--- modelowanie/src/scripts/canvas/CuttingCurve/Trimming.js
+++ modelowanie/src/scripts/canvas/CuttingCurve/Trimming.js
@@ -30,8 +30,8 @@ export function trimIsMet(u, v, ops) {
 }
 export function findTorusBoundryToTrim(u1, v1, u2, v2, ops) {
     const division = 100;
-    const diff1 = (u2 - u1) / division;
-    const diff2 = (v2 - v1) / division;
+    const diff1 = Math.abs(u2 - u1) < 0.5 ? (u2 - u1) / division : (1 - u1 - u2) / division;
+    const diff2 = Math.abs(v2 - v1) < 0.5 ? (v2 - v1) / division : (1 - v1 - v2) / division;
     let loopU = u1;
     let loopV = v1;
     let loopPrevU = u1;
@@ -40,28 +40,41 @@ export function findTorusBoundryToTrim(u1, v1, u2, v2, ops) {
     for(let i = 0; i < division; i ++) {
         loopU += diff1;
         loopV += diff2;
+        if(loopV < 0) {
+            loopU = 0.99999;
+        }
+        if(loopU > 1) {
+            loopU = 0;
+        }
+
         let y = parseInt(loopU * 500, 10);
         let x = parseInt(loopV * 500, 10);
         y = y === 500 ? 499 : y;
         x = x === 500 ? 499 : x;
-
+        if(Math.abs(u2 - u1) > 0.5 || Math.abs(v2 - v1) > 0.5) {
+            console.log(x, y);
+        }
         const place1 = (parseInt((y), 10)* canvas.width * 4) + (parseInt(x, 10) * 4);
-        console.log(place1);
 
         y = parseInt(loopPrevU * 500, 10);
         x = parseInt(loopPrevV * 500, 10);
         y = y === 500 ? 499 : y;
         x = x === 500 ? 499 : x;
+        if(Math.abs(u2 - u1) > 0.5 || Math.abs(v2 - v1) > 0.5) {
+            console.log(parseInt(v2 * 500, 10), parseInt(u2 * 500, 10));
+        }
     
         const place2 = (parseInt((y), 10)* canvas.width * 4) + (parseInt(x, 10) * 4);
 
         if(op === "left") {
-            if(img.data[place1] === 255 && img.data[place1] === 255 && img.data[place1] === 255) {
+            if((img.data[place1] === 255 && img.data[place1] === 255 && img.data[place1] === 255)
+            || (img.data[place1] === 255 && img.data[place1] === 0 && img.data[place1] === 0)) {
                 break;
             }
         }
         if(op === "right") {
-            if(img.data[place1] === 0 && img.data[place1] === 0 && img.data[place1] === 0) {
+            if((img.data[place1] === 0 && img.data[place1] === 0 && img.data[place1] === 0)
+            || (img.data[place1] === 255 && img.data[place1] === 0 && img.data[place1] === 0)) {
                 break;
             }
         }
@@ -70,6 +83,9 @@ export function findTorusBoundryToTrim(u1, v1, u2, v2, ops) {
         }
         loopPrevU = loopU;
         loopPrevV = loopV;
+    }
+    if(parseInt(500 * loopPrevU  - 500 * u1, 10) > 200 || parseInt(500 * loopPrevV - 500 * v2, 10) > 200) {
+        console.log(u1, u2, v1, v2);
     }
     const ret = {u: loopPrevU, v: loopPrevV};
     return ret;
